32120fd25737183a11afc39646690d97db87fcb5
Oka Sigit <oka.sigit9@gmail.com>
2023-06-29 13:50:09 +0700

    improvement

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('level');
+            $table->boolean('is_email_verified')->default(0);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
